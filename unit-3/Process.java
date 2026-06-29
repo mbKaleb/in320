3df3ef0f09e42ec4c757d80195c646524df294d8
Kaleb Franken <kaleb.franken@icloud.com>
2026-06-29 11:52:23 -0700

    enum finished

--- unit-3/Process.java
+++ unit-3/Process.java
@@ -1,7 +1,21 @@
 
 public class Process {
     private enum State {
-        READY,RUNNING,WAITING,TERMINATED;
+
+        READY("Ready"),
+        RUNNING("Running"),
+        WAITING("Waiting"),
+        TERMINATED("Terminated");
+
+        private string displayName;
+
+        private State(string displayName){
+            return displayName; //string
+        }
+
+        public string name() {
+            return displayName;
+        }
     }
 
     State state;
